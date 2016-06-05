cb01b93c6bb3ea42e79359320f753f4e9aedf8a2
Daniel Bayley <daniel.bayley@me.com>
2016-06-05 23:40:05 +0100

    Backup icons using zip archive
    Backup original icons as `.zip` archives instead of merely suffixing `.bak` to copies.

--- refre.sh
+++ refre.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env zsh -f
+unsetopt nomatch
 
 cd $NODE_PATH:h:h
 
@@ -8,12 +9,12 @@ then [ -d $1 ] && set -- $1/{atom,file}.icns
 	png=(*/resources/*.png)
 
 	# Backup existing icon/s
-	for i (*.icns $png) mv -n $i{,.bak}
+	for i (*.icns $png) [ -f $i.zip ] || zip -jmq $i{.zip,}
 
-	cp -f $@ . #:l
+	cp $@ . #:l
 
 	sips -s format png $1:t -o $png
 else
 	# Restore original icon/s
-	for i (**/*.bak) mv $i ${i/.bak/}
+	for i (**/*.zip) unzip -oqd $i:h $i && rm $_
 fi
