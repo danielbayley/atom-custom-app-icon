eb25f7f100112a5e56927648ac965c3b398fe32f
Daniel Bayley <daniel.bayley@me.com>
2016-06-05 23:34:23 +0100

    Bug fix
    Prevent creation of multiple backup files.

--- refre.sh
+++ refre.sh
@@ -8,12 +8,12 @@ then [ -d $1 ] && set -- $1/{atom,file}.icns
 	png=(*/resources/*.png)
 
 	# Backup existing icon/s
-	for i (*.icns $png) mv $i $i:r.bak.$i:e
+	for i (*.icns $png) mv -n $i{,.bak}
 
 	cp -f $@ . #:l
 
 	sips -s format png $1:t -o $png
 else
 	# Restore original icon/s
-	for i (**/*.bak.*) mv $i ${i/.bak/}
+	for i (**/*.bak) mv $i ${i/.bak/}
 fi
