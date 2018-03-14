f21d924d776a4020bff8bd4b7b18dcaeb4ad3da0
Daniel Bayley <daniel.bayley@me.com>
2018-03-14 23:19:40 +0000

    Tidy

--- refre.sh
+++ refre.sh
@@ -1,14 +1,16 @@
-#! /bin/zsh -f --extended-glob --glob-assign +o nomatch
+#! /bin/zsh -f --extended-glob --glob-assign
+set -- ${@:-$ATOM_HOME}
 
-cd ${NODE_PATH/.app*/}.app/Contents/Resources
+pushd ${NODE_PATH/.app*/}.app/Contents/Resources
 
 if (($#))
-then [ -d $1 ] && set -- $1/{atom,file}.icns
+then [ -d $1 ] && set -- $1/{atom,file}.icns(N)
   png=*/resources/*.png
 
   # Backup existing icon/s
   for i (*.icns $png) [ -f $i.zip ] || zip -jmq $i{.zip,}
-  cp $@ . #:l
+
+  cp $@ $PWD
   sips -s format png $1:t --out $png
 else
   # Restore original icon/s
